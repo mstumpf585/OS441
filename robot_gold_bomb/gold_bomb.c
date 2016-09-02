fdca0cb900095d718ebeab19dc0d59502134e382
kalvin66rocks <kalvin66rocks@gmail.com>
2016-09-02 08:26:02 -0700

    random functions

--- robot_gold_bomb/gold_bomb.c
+++ robot_gold_bomb/gold_bomb.c
@@ -28,6 +28,61 @@ void print_grid(struct workspace *workspace_data){
 
 }
 
+void randomBomb(struct workspace *workspace_data);
+void randomBomb(struct workspace *workspace_data)
+{
+	int random [2];
+	for(int i =0; i<2;i++)
+	{
+		random[i] = getRandom(0,3);
+	}
+	workspace_data->grid[random[0]][random[1]] = 'b';
+}
+
+void randomGold(struct workspace *workspace_data);
+void randomGold(struct workspace *workspace_data)
+{
+	int random [2];
+	bool conflicts = true;
+
+	for(int i = 0; i < 2; i++)
+	{
+		while(conflicts == true)
+		{
+			for(int i =0; i<2;i++)
+			{
+				random[i] = getRandom(0,3);
+			}
+			if(workspace_data->grid[random[0]][random[1]] != 'b' && workspace_data->grid[random[0]][random[1]] != 'g')
+			{
+				conflicts = false;
+			}
+		}
+		workspace_data->grid[random[0]][random[1]] = 'g';
+	}
+}
+
+void randomRobot(struct workspace *workspace_data);
+void randomRobot(struct workspace *workspace_data)
+{
+	int random [2];
+	bool conflicts = true;
+
+	while(conflicts == true)
+	{
+		for(int i =0; i<2;i++)
+		{
+			random[i] = getRandom(0,3);
+		}
+		if(workspace_data->grid[random[0]][random[1]] != 'b' && workspace_data->grid[random[0]][random[1]] != 'g')
+		{
+			conflicts = false;
+		}
+	}
+	workspace_data->grid[random[0]][random[1]] = 'r';
+
+}
+
 void make_workspace(struct workspace *workspace_data);
 void make_workspace(struct workspace *workspace_data){
 
@@ -39,9 +94,14 @@ void make_workspace(struct workspace *workspace_data){
 		}
 	}
 
+	randomBomb(workspace_data);
+	randomGold(workspace_data);
+	randomRobot(workspace_data);
 	print_grid(workspace_data);
 
 }
+
+
 
 //function: main
 int main(int argc, char* argv[])
