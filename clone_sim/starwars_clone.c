12d2a4990b7c76565dc0f30b35e847ee5551af8f
Michael <pro585g@gmail.com>
2016-09-14 17:36:14 -0400

    we done

--- clone_sim/starwars_clone.c
+++ clone_sim/starwars_clone.c
@@ -5,20 +5,84 @@
 
 struct data_clone{
 
-	int ID;
+	int clone_ID;
 	char name[30];
 	int timeCounter;
-
 };
 
 int  create_clone(struct data_clone *clone);
 void input(struct data_clone *clone, int user_size);
-void life_span(struct data_clone *clone);
+void life_span(struct data_clone *clone, int user_size);
+
+void life_span(struct data_clone *clone, int user_size){
+
+	int break_flag = 0;
+
+	while(1){
+
+		for(int i=0; i<user_size; i++){
+
+			if(clone[i].timeCounter > 0){
+
+				break_flag++;
+
+				printf("clone ID: %d \n", clone[i].clone_ID);
+				printf("clone Name: %s", clone[i].name);
+				printf("clone Life Span %d \n\n", clone[i].timeCounter);
+
+				clone[i].timeCounter--;
+			}else{
+
+
+				printf("%sis dead\n\n",clone[i].name);
+			}
+		}
+		if(break_flag != 0){
+
+			break_flag = 0;
+		}else{
+
+			break;
+		}
+	}
+
+
+
+}
+void input(struct  data_clone *clone, int user_size){
+
+        char temp_name[30];
+        int c;
+        int temp;
+
+	printf("in input user size is %d \n", user_size);
+	for(int i=0; i<user_size; i++){
+
+		printf("enter clone ID: ");
+		scanf("%d", &temp);
+		clone[i].clone_ID = temp;
+
+		printf("enter clone name: ");
+		while((c = fgetc(stdin)) != EOF && c != '\n');
+		fgets(clone[i].name, 30, stdin);
+		sscanf(clone[i].name,"%s",&temp_name);
+
+		temp = 0;
+
+		printf("enter lifespan: ");
+		scanf("%d", &temp);
+		clone[i].timeCounter = temp;
+
+		printf("\n");
+        }
+}
 
 int create_clone(struct data_clone *clone){
 
 	int user_size = 0;
+
         scanf("%d", &user_size);
+
         clone = (struct data_clone*) malloc(user_size*sizeof(struct data_clone));
 
 	if(clone == NULL){
@@ -30,38 +94,15 @@ int create_clone(struct data_clone *clone){
 		printf("worked \n");
 	}
 
+	input(clone, user_size);
+	life_span(clone,user_size);
 	return user_size;
-}
-
-void input(struct  data_clone *clone, int user_size){
-
-	char temp_name[30];
-	int c;
-	int temp;
-
-	printf("Clone name: \n");
-
-	for(int i; i<user_size; i++){
-
-		scanf("enter ID %d", &temp);
-		clone[i].ID = temp;
-
-		while((c = fgetc(stdin)) != EOF && c != '\n');
-		fgets(clone->name, 30, stdin);
-		sscanf(clone->name,"%s",&temp_name);
-
-		temp = 0;
-		scanf("enter life span %d", &temp);
-		clone[i].timeCounter = temp;
-	}
 }
 
 int main(int argc, char* argv[]){
 
 	struct data_clone *Aclone;
-
 	int temp_num;
 
 	temp_num = create_clone(Aclone);
-	input(Aclone, temp_num);
 }
